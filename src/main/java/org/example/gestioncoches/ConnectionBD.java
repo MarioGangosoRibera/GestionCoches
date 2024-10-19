e41cc14e55ea1f1554d10e1c4dd96ca3bee8f6e9
Mario <mario.ganfer@educa.jcyl.es>
2024-10-19 15:47:43 +0200

    Hecha la conexion a la base de datos y el boton insertar

--- src/main/java/org/example/gestioncoches/ConnectionBD.java
+++ src/main/java/org/example/gestioncoches/ConnectionBD.java
@@ -1,20 +1,39 @@
 package org.example.gestioncoches;
 
-public class ConnectionBD {
-    public static MongoClient conectar() {
-        try {
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 
-            final MongoClient conexion = new MongoClient(new MongoClientURI("mongodb://admin:admin@localhost:27017/?authSource=admin"));
+import java.util.Properties;
+
+public class ConnectionBD {
+    public static MongoClient getConexion() {
+       /* try {
+
+            final MongoClient conexion = new MongoClient(new MongoClientURI("mongodb://admin:1234@localhost:27017/?authSource=admin"));
+
             System.out.println("Conectada correctamente a la BD");
+
             return conexion;
         } catch (Exception e) {
             System.out.println("Conexion Fallida");
             System.out.println(e);
             return null;
         }
-    }
+        */
 
-    public static void desconectar(MongoClient con) {
-        con.close();
+
+        try {
+            Properties prop = new Properties();
+            prop.load(R.getProperties("database.properties"));
+            String host = prop.getProperty("host");
+            int port = Integer.parseInt(prop.getProperty("port"));
+            String username = prop.getProperty("username");
+            String password = prop.getProperty("password");
+            return new MongoClient(new MongoClientURI("mongodb://" + username + ":" + password + "@" + host + ":" + port + "/?authSource=admin"));
+
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
     }
 }
--- src/main/java/org/example/gestioncoches/HelloController.java
+++ src/main/java/org/example/gestioncoches/HelloController.java
@@ -1,14 +1,116 @@
 package org.example.gestioncoches;
 
+import com.mongodb.ErrorCategory;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoWriteException;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import org.bson.Document;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import static org.example.gestioncoches.CocheDAO.collectionTipos;
+import static org.example.gestioncoches.CocheDAO.crearCoche;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
+
+    MongoClient con;
+    ArrayList<String> listaTipos = new ArrayList<>(Arrays.asList("Gasolina", "Diesel", "Híbrido", "Eléctrico"));
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField txtMatricula;
+
+    @FXML
+    private Button btnNuevo;
+
+    @FXML
+    private TextField txtMarca;
+
+    @FXML
+    private TextField txtModelo;
+
+    @FXML
+    private Button btnInsertar;
+
+    @FXML
+    private Button btnModificar;
+
+    @FXML
+    private Button btnEliminar;
+
+    @FXML
+    private TableView<?> tvTabla;
+
+    @FXML
+    private TableColumn<?, ?> tcMatricula;
+
+    @FXML
+    private TableColumn<?, ?> tcMarca;
+
+    @FXML
+    private TableColumn<?, ?> tcModelo;
+
+    @FXML
+    private TableColumn<?, ?> tcTipo;
+
+    @FXML
+    private ComboBox<String> cbTipo;
+
+    @FXML
+    void clicEliminar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clicInsertar(ActionEvent event) {
+        String matricula = txtMatricula.getText();
+        String marca = txtMarca.getText();
+        String modelo = txtModelo.getText();
+        String tipo = cbTipo.getValue();
+        Coche coche = new Coche(matricula, marca, modelo, tipo);
+        Alerta.mostrarAlerta(crearCoche(coche));
+    }
+
+    @FXML
+    void clicModificar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clicLimpiar(ActionEvent event) {
+
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        con = ConnectionBD.getConexion();
+        MongoDatabase database = con.getDatabase("CrudCoches");
+        // Me devuelve una colección si no existe la crea
+        CocheDAO.collectionCoches = database.getCollection("coches");
+        collectionTipos = database.getCollection("tipos");
+        Document tipos = new Document();
+        tipos.append("tipos", listaTipos);
+        cbTipo.getItems().addAll(listaTipos);
+        try {
+            //La función ".insertOne()" se utiliza para insertar el documento en la colección.
+            collectionTipos.insertOne(tipos);
+        } catch (MongoWriteException mwe) {
+            if (mwe.getError().getCategory().equals(ErrorCategory.DUPLICATE_KEY)) {
+                System.out.println("El documento con esa identificación ya existe");
+            }
+        }
+
     }
 }
